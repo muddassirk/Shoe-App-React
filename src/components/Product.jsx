25637d1c70300680825dfa6ac78e275df96b1bf4
muddassirk <mkkhanu72@gmail.com>
2021-05-04 02:12:36 +0500

    products added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import Shoe from '../shoe.json'
+import '../App.css'
+import { Link } from 'react-router-dom';
 
 export default function Product() {
+    console.log(Shoe);
+    console.log(Object.keys(Shoe));
     return (
         <div>
-            this is product
+            <h1>Welcome to prodcuts</h1>
+            <div className='products'>
+                {
+                    Object.keys(Shoe).map(keyNames => {
+                        const shoes = Shoe[keyNames]
+                        return (
+                            <Link
+                                key={keyNames}
+                                className='div'
+                                to={`/product/${keyNames}`}
+                            >
+
+                                    <h1>{shoes.name}</h1>
+                                    <img src={shoes.img} height={150} alt="" />
+                            </Link>
+                        )
+
+                    })
+                }
+            </div>
         </div>
     )
 }
