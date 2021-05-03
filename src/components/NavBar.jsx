d0bedd98dce4556b4d0346da9e927bea5a0e7d98
muddassirk <mkkhanu72@gmail.com>
2021-05-04 02:12:20 +0500

    className plus styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import '../App.css'
 export default function NavBar() {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/product'>Products</Link>
+        <div className='navBar'>
+            <Link className='navBarItems' to='/'>Home</Link>
+            <Link className='navBarItems' to='/about'>About</Link>
+            <Link className='navBarItems' to='/product'>Products</Link>
         </div>
     )
 }
