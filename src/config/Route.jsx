714bdbcfab88f5f6178c7d487834da676a8ad703
muddassirk <mkkhanu72@gmail.com>
2021-05-04 00:53:31 +0500

    routing complete with dynamically

--- src/config/Route.jsx
+++ src/config/Route.jsx
@@ -4,15 +4,21 @@ import {
     Switch,
     Route
 } from 'react-router-dom'
+import Home from '../components/Home'
+import About from '../components/About'
+import Product from '../components/Product'
+import ProductItems from '../components/ProductItems'
 
-export default function Route() {
+export default function Rout() {
     return (
         <div>
             <Router>
                 <Switch>
-                    <Route path='/' component={} />
-                    <Route path='/about' component={} />
-                    <Route path='/product' component={} />
+                    <Route exact path='/' component={Home} />
+                    <Route path='/about' component={About} />
+                    <Route exact path='/product' component={Product} />
+                    <Route path='/product/:id' component={ProductItems} />
+                    <Route path='*' component={()=><h1>404 Not Found</h1>} />
                 </Switch>
             </Router>
             
