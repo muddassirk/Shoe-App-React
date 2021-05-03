db7a35c038c353a64080967a5c65c1531a364a57
muddassirk <mkkhanu72@gmail.com>
2021-05-04 02:13:03 +0500

    dynamic routes plus useParams

--- src/components/ProductItems.jsx
+++ src/components/ProductItems.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import Shoe from '../shoe.json'
+
 
 export default function ProductItems() {
+
+    const { id } = useParams()
+    const shoe = Shoe[id]
+    console.log(shoe);
+    if (!shoe) return <h1>Product not Found</h1>
+
     return (
         <div>
             this is product items
+            <div
+                className='div'
+            >
+
+                <h1>{shoe.name}</h1>
+                <img src={shoe.img} height={500} alt="" />
+            </div>
         </div>
     )
 }
