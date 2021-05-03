374197757af335eb958c2cada1824c2a40147255
muddassirk <mkkhanu72@gmail.com>
2021-05-04 01:08:09 +0500

    created navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function NavBar() {
+    return (
+        <div>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/product'>Products</Link>
+        </div>
+    )
+}
